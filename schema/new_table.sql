45db45865c7a6c18d6c5279c6979f4d4173948dc
Frans <franswhyburd@gmail.com>
2017-06-15 05:50:14 +0200

    first other

---
+++ schema/new_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `new_table` (
+  `idnew_table` int(11) NOT NULL AUTO_INCREMENT,
+  `new_tablecol` varchar(45) DEFAULT NULL,
+  `new_tablecol1` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idnew_table`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
